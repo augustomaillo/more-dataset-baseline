ffbec9c0880467517b05a342b56aa142d0d4c96c
augustomaillo <augusto.maillo@gmail.com>
2020-10-23 14:31:00 -0300

    Updating evaluation functions

--- utils/generator.py
+++ utils/generator.py
@@ -8,28 +8,26 @@ import cv2
 from utils.dataset import Dataset
 import scipy.spatial.distance
 from tensorflow.keras.applications.resnet import preprocess_input
-
+from itertools import cycle
 
 def general_generator( 
     dataset, 
     batch_size = 4, 
-    validation = False, 
-    val_ident_num = 0, 
+    partition = 'train',  
     aug = True, 
     img_size = (224,320), 
     label_smoothing = True
     ):
 
-    data_camA = dataset.content_array('camA')[0]
-    data_camB = dataset.content_array('camB')[0]
-    idA = dataset.content_array('camA')[1]
-    idB = dataset.content_array('camB')[1]
-    ident_num = dataset.ident_num()
+    data_camA, idA = dataset.content_array(partition =partition, cam_name ='camA')
+    data_camB, idB = dataset.content_array(partition =partition, cam_name ='camB')
+    ident_num = dataset.ident_num(partition)
 
-    if validation:
+    if partition != 'train':
         idA = idA%ident_num
         idB = idB%ident_num
         label_smoothing = False
+        aug = False
 
     while True:
     
@@ -68,14 +66,11 @@ def general_generator(
         for pA, iA in zip(dataA, pidA):
             imgA = dataset.get_image(pA)
             imgA = Image.fromarray(imgA)
-            
             imgA = imgA.resize((img_size[1], img_size[0]))
 
             if aug:
                 imgA = data_augmentation.augment(imgA,['brightness', 'cutout', 'zooming', 'horizontal_flip', 'rotate', 'translate'])
 
-    #       print(type(imgA))
-            # conjunto de imagens a serem consideradas
             imgA = np.array(imgA)
             imgA = preprocess_input(imgA)
             imgA /= 255
@@ -85,7 +80,6 @@ def general_generator(
 
 
         for pB, iB in zip(dataB, pidB):
-            
             imgB = dataset.get_image(pB)
             imgB = Image.fromarray(imgB)
             imgB = imgB.resize((img_size[1], img_size[0]))
@@ -93,7 +87,6 @@ def general_generator(
             if aug:
                 imgB = data_augmentation.augment(imgB,['brightness','cutout','zooming', 'horizontal_flip', 'rotate', 'translate'])
            
-            # conjunto de imagens a serem consideradas
             imgB = np.array(imgB)
             imgB = preprocess_input(imgB)
             imgB /= 255
@@ -101,7 +94,7 @@ def general_generator(
             idB_set.append(np_utils.to_categorical(iB-1,ident_num))  
 
 
-        y_true = np.zeros(batch_size) # não tem significado, apenas para não dar erro
+        y_true = np.zeros(batch_size) # no meaning, just to fit batch shape
         
         if(len(camA_set) < len(camB_set)):
             for i in range(len(camB_set) - len(camA_set)):
@@ -122,18 +115,16 @@ def general_generator(
         cls_idB_set = np.array(idB_set)
 
         if(label_smoothing):
-           ## LABEL SMOOTHING
             epsilon = 0.1
             cls_idA_set[np.where(cls_idA_set == 0)] = epsilon/ident_num
             cls_idA_set[np.where(cls_idA_set == 1)] = 1 - ( (ident_num-1)/ident_num)*0.1
             
             cls_idB_set[np.where(cls_idB_set == 0)] = epsilon/ident_num
             cls_idB_set[np.where(cls_idB_set == 1)] = 1 - ( (ident_num-1)/ident_num)*0.1
-            ####
         
-        if validation:
-            cls_idA_set = np.zeros((cls_idA_set.shape[0], val_ident_num))
-            cls_idB_set = np.zeros((cls_idB_set.shape[0], val_ident_num))
+        if partition != 'train':
+            cls_idA_set = np.zeros((cls_idA_set.shape[0], itent_num))
+            cls_idB_set = np.zeros((cls_idB_set.shape[0], itent_num))
 
         
         yield([
@@ -154,24 +145,21 @@ def general_generator(
 def general_generator_center(
     dataset, 
     batch_size = 4, 
-    validation = False, 
-    val_ident_num = 0, 
+    partition = 'train',  
     aug = True, 
     img_size = (224,320), 
     label_smoothing = True
     ):
-    data_camA = dataset.content_array('camA')[0]
-    data_camB = dataset.content_array('camB')[0]
-    idA = dataset.content_array('camA')[1]
-    idB = dataset.content_array('camB')[1]
-    ident_num = dataset.ident_num()
 
+    data_camA, idA = dataset.content_array(partition =partition, cam_name ='camA')
+    data_camB, idB = dataset.content_array(partition =partition, cam_name ='camB')
+    ident_num = dataset.ident_num(partition)
 
-    if validation:
+    if partition != 'train':
         idA = idA%ident_num
         idB = idB%ident_num
         label_smoothing = False
-
+        aug = False
 
     while True:
     
@@ -180,9 +168,6 @@ def general_generator_center(
         idA_set = []
         idB_set = []
 
-        # idA_num = []
-        # idB_num = []
-        # embaralhando os dados para gerar conjuntos diferentes
         uIDs = np.unique(idA)
         random.shuffle(uIDs)
         posA = []
@@ -211,22 +196,16 @@ def general_generator_center(
         for pA, iA in zip(dataA, pidA):
             imgA = dataset.get_image(pA)
             imgA = Image.fromarray(imgA)
-            
             imgA = imgA.resize((img_size[1], img_size[0]))
 
             if aug:
                 imgA = data_augmentation.augment(imgA,['brightness', 'cutout', 'zooming', 'horizontal_flip', 'rotate', 'translate'])
 
-    #       print(type(imgA))
-            # conjunto de imagens a serem consideradas
             imgA = np.array(imgA)
             imgA = preprocess_input(imgA)
             imgA /= 255
             camA_set.append(imgA)
             idA_set.append(np_utils.to_categorical(iA-1,ident_num))  
-            # idA_num.append(iA-1)
-
-
 
         for pB, iB in zip(dataB, pidB):
             imgB = dataset.get_image(pB)
@@ -236,15 +215,13 @@ def general_generator_center(
             if aug:
                 imgB = data_augmentation.augment(imgB,['brightness','cutout','zooming', 'horizontal_flip', 'rotate', 'translate'])
            
-            # conjunto de imagens a serem consideradas
             imgB = np.array(imgB)
             imgB = preprocess_input(imgB)
             imgB /= 255
             camB_set.append(imgB)
             idB_set.append(np_utils.to_categorical(iB-1,ident_num))  
-            # idB_num.append(iB-1)
 
-        y_true = np.zeros(batch_size) # não tem significado, apenas para não dar erro
+        y_true = np.zeros(batch_size)
         
         if(len(camA_set) < len(camB_set)):
             for i in range(len(camB_set) - len(camA_set)):
@@ -264,18 +241,16 @@ def general_generator_center(
         cls_idB_set = np.array(idB_set)
 
         if(label_smoothing):
-           ## LABEL SMOOTHING
             epsilon = 0.1
             cls_idA_set[np.where(cls_idA_set == 0)] = epsilon/ident_num
             cls_idA_set[np.where(cls_idA_set == 1)] = 1 - ( (ident_num-1)/ident_num)*0.1
             
             cls_idB_set[np.where(cls_idB_set == 0)] = epsilon/ident_num
             cls_idB_set[np.where(cls_idB_set == 1)] = 1 - ( (ident_num-1)/ident_num)*0.1
-            ####
         
-        if validation:
-            cls_idA_set = np.zeros((cls_idA_set.shape[0], val_ident_num))
-            cls_idB_set = np.zeros((cls_idB_set.shape[0], val_ident_num))
+        if partition != 'train':
+            cls_idA_set = np.zeros((cls_idA_set.shape[0], itent_num))
+            cls_idB_set = np.zeros((cls_idB_set.shape[0], itent_num))
 
         
         yield([
@@ -297,10 +272,10 @@ def general_generator_center(
                 np.zeros(len(cls_idA_set))
             ])   
 
-from itertools import cycle
 def classification_generator(
  dataset,
- batch_size = 32, 
+ batch_size = 32,
+ partition = 'train',
  aug = True, 
  img_size = (224,320), 
  label_smoothing = True
@@ -316,26 +291,24 @@ def classification_generator(
     Returns:
             a generator object
     """  
-
-    # def get_image(dataset_file, img_name):
-    #     with h5py.File(dataset_file, 'r') as f:
-    #         return f['images'][img_name].value, f['images'][img_name].attrs['ID']
-
-    # with h5py.File(dataset_file, 'r') as f:
-    #     all_train_ids = f['metadata']['train_ids'].value.tolist()
-
-    data_camA, idA = dataset.content_array(partition ='train', cam_name ='camA')
-    data_camB, idB = dataset.content_array(partition ='train', cam_name ='camB')
+    data_camA, idA = dataset.content_array(partition =partition, cam_name ='camA')
+    data_camB, idB = dataset.content_array(partition =partition, cam_name ='camB')
 
     all_images_names = np.concatenate([data_camA, data_camB])
     all_ids_list = np.concatenate([idA,idB])
-    ident_num = dataset.ident_num('train')
+    ident_num = dataset.ident_num(partition)
 
     all_train_ids = np.unique(idA)
 
     np.random.shuffle(all_train_ids)
     pool = cycle(all_train_ids)
-    
+
+    if partition != 'train':
+        idA = idA%ident_num
+        idB = idB%ident_num
+        label_smoothing = False
+        aug = False
+
     current_ids = []
     while True:
         current_ids = []
@@ -348,7 +321,6 @@ def classification_generator(
         img_batch = []
         labels_batch = []
         
-
         # yield( None)
         img_data = []
         labels_list = []
@@ -358,43 +330,36 @@ def classification_generator(
                     img_data.append(s_)
                     labels_list.append(c_)
 
-        # print(img_data, labels_list)
-
-
-
-
         for img_file, lb in zip(img_data, labels_list):
-                img = dataset.get_image(img_file)
-                img = cv2.resize(img, dsize=(img_size[1], img_size[0]), interpolation=cv2.INTER_CUBIC)
-                if aug:
-                    img = data_augmentation.augment(img,['zooming','translate','rotate','horizontal_flip','brightness','cutout'])
+            img = dataset.get_image(img_file)
+            img = cv2.resize(img, dsize=(img_size[1], img_size[0]), interpolation=cv2.INTER_CUBIC)
+            if aug:
+                img = data_augmentation.augment(img,['zooming','translate','rotate','horizontal_flip','brightness','cutout'])
+        
+            img = np.array(img)
+            img = preprocess_input(img)
+            img /= 255
+            img_batch.append(img)
+            label_ = np.array(np_utils.to_categorical(lb-1,ident_num))
             
-                img = np.array(img)
-                img = preprocess_input(img)
-                img /= 255
-                # conjunto de imagens a serem consideradas
-                img_batch.append(img)
-                label_ = np.array(np_utils.to_categorical(lb-1,ident_num))
-                
-                ## LABEL SMOOTHING
+            ## LABEL SMOOTHING
+            if label_smoothing:
+                epsilon = 0.1
+                label_[np.where(label_ == 0)] = epsilon/ident_num
+                label_[np.where(label_ == 1)] = 1 - ( (ident_num-1)/ident_num)*0.1
+            ####
+            
 
-                if label_smoothing:
-                    epsilon = 0.1
-                    label_[np.where(label_ == 0)] = epsilon/ident_num
-                    label_[np.where(label_ == 1)] = 1 - ( (ident_num-1)/ident_num)*0.1
-                ####
-                
+            labels_batch.append(label_)
+            
+            if len(img_batch) == batch_size:
 
-                labels_batch.append(label_)
-                
-                if len(img_batch) == batch_size:
+                if len(np.shape(labels_batch))==3:
 
-                    if len(np.shape(labels_batch))==3:
+                    out = np.squeeze(np.array(labels_batch), axis=1)
+                else:
+                    out = np.array(labels_batch)
+                yield(np.array(img_batch), out)
 
-                        out = np.squeeze(np.array(labels_batch), axis=1)
-                    else:
-                        out = np.array(labels_batch)
-                    yield(np.array(img_batch), out)
-
-                    img_batch = []
-                    labels_batch = []
+                img_batch = []
+                labels_batch = []
--- utils/test_metrics.py
+++ utils/test_metrics.py
@@ -4,144 +4,113 @@ from PIL import Image
 from keras.models import Model
 import scipy.spatial.distance
 from sklearn.metrics import average_precision_score
-from model import resnet_b4_max_pool as new_resnet
+from tensorflow.keras.applications.resnet import preprocess_input
+from multiprocessing.pool import ThreadPool
+from model.AuxLayers import BNNeckForInference
 
+class EvalModel():
+        def __init__(self, dataset, partition, image_shape):
+            print('Loading train data on RAM.')
+            self.image_shape = image_shape
+            test_dataA, self.ids_camA = dataset.content_array(partition = partition, cam_name ='camA')
+            test_dataB, self.ids_camB = dataset.content_array(partition = partition, cam_name ='camB')
+            self.camA_set = []
+            self.camB_set = []
+            
+            def process_image(img_name):
+                imgA = dataset.get_image(img_name)
+                imgA = Image.fromarray(imgA)
+                imgA = imgA.resize((self.image_shape[1], self.image_shape[0]))
+                imgA = np.array(imgA)
+                imgA = preprocess_input(imgA)
+                imgA /= 255
+                return imgA
 
-def NormalizeData(data):
-    return (data - np.min(data)) / (np.max(data) - np.min(data))
+            processA = ThreadPool(4).map(process_image, test_dataA)
+            for r in processA:
+                self.camA_set.append(r)
+                print(len(self.camA_set), '/',len(test_dataA), end='\r')
+            self.camA_set = np.array(self.camA_set)
+            print()
+            processB = ThreadPool(4).map(process_image, test_dataB)
+            for r in processB:
+                self.camB_set.append(r)
+                print(len(self.camB_set), '/', len(test_dataB), end='\r')
+            self.camB_set = np.array(self.camB_set)
 
-'''
-def generate_cmc_curve(feat_model, test_data_list, name='cmc_curve', image_size =(224,320),metrics = ['r1, mAP'], curve_label = 'Test', curve_color='red' ):
-    test_dataA, test_identA, test_dataB, test_identB = test_data_list
+        def compute(self, 
+            feat_model,
+            name='cmc_curve', 
+            metrics = ['r1, mAP'], 
+            curve_label = 'Evaluating', 
+            curve_color='red', 
+            BN = False
+        ):                       
+            return generate_cmc_curve(
+                feat_model,
+                self.camA_set,
+                self.camB_set,
+                self.ids_camA,
+                self.ids_camB,
+                name = name,
+                image_size = self.image_shape,
+                metrics = metrics,
+                curve_label = curve_label,
+                curve_color = curve_color,
+                BN = BN,
+                )
 
-    camA_set0 = []
-    camB_set0 = []
-      
-    # feat_model = new_resnet.feat_net((224,320)) # prev layer
-    # feat_model._make_predict_function()    # https://github.com/keras-team/keras/issues/6462
-    # feat_model.load_weights(feat_path)
+def generate_cmc_curve(
+        feat_model, 
+        camA_set,
+        camB_set, 
+        ids_camA,
+        ids_camB,
+        name='cmc_curve', 
+        image_size = (224,320),
+        metrics = ['r1, mAP'], 
+        curve_label = 'On Train Data', 
+        curve_color='red', 
+        BN = False
+        ):
 
-    for pA in test_dataA:
-      imgA = np.asarray(Image.open(pA)).copy()
-      imgA = Image.fromarray(imgA)
-      imgA = imgA.resize((image_size[1], image_size[0]))
-      camA_set0.append(np.array(imgA))
+        metric = 'euclidean'
+        if not BN:
+            probe_features = feat_model.predict(camA_set, verbose=1)
+            gallery_features = feat_model.predict(camB_set, verbose=1)
+        else:
+            print('BN active for inference. Metric for inference changed to cosine.')
+            bnneck = BNNeckForInference(feat_model, image_size)
+            probe_features = bnneck.predict(camA_set, verbose=1)
+            gallery_features = bnneck.predict(camB_set, verbose=1)
+            metric = 'cosine'
 
-    camA_set = np.array(camA_set0)
+        cmc_curve = np.zeros(gallery_features.shape[0])
+        ap_array = []
+        all_dist = scipy.spatial.distance.cdist(probe_features, gallery_features, metric=metric)
 
-    for pB in test_dataB:
-      imgB = np.asarray(Image.open(pB)).copy()
-      imgB = Image.fromarray(imgB)
-      imgB = imgB.resize((image_size[1], image_size[0]))
-      camB_set0.append(np.array(imgB)) 
+        for idx, p_dist in enumerate(all_dist):
+            rank_p = np.argsort(p_dist,axis=None)
+            ranked_ids = ids_camB[rank_p]
+            pos = np.where(ranked_ids == ids_camA[idx])
+            cmc_curve[pos[0][0]]+=1
+            y_true = np.zeros(np.shape(ids_camB))
+            y_true[np.where(ids_camB == ids_camA[idx])] = 1
+            y_pred = 1/(p_dist + 1e-8) # remove /0
+            y_pred = np.squeeze(y_pred)
+            ap = average_precision_score(y_true, y_pred)
+            ap_array.append(ap)
+        cmc_curve = np.cumsum(cmc_curve)/probe_features.shape[0]
 
-    camB_set = np.array(camB_set0)
+        import matplotlib
+        matplotlib.use('Agg')
+        from matplotlib import pyplot
+        pyplot.title('CMC Curve - R1: %.2f / mAP: %.2f'%(cmc_curve[0]*100, np.mean(ap_array)*100) )
+        pyplot.ylabel('Recognition Rate (%)')
+        pyplot.xlabel('Rank')
+        pyplot.plot(cmc_curve,label = curve_label, color=curve_color)
+        pyplot.legend(loc='upper left')
+        pyplot.savefig(name+'.png')
+        pyplot.close()
+        return cmc_curve, cmc_curve[0], np.mean(ap_array)
 
-    ids_camA = np.array(test_identA)
-    ids_camB = np.array(test_identB)
-
-    probe_features = feat_model.predict(camA_set)
-    gallery_features = feat_model.predict(camB_set)
-
-    cmc_curve = np.zeros(gallery_features.shape[0])
-    ap_array = []
-
-    for idx, probe in enumerate(probe_features):
-
-      rank_p = np.argsort(scipy.spatial.distance.cdist(np.expand_dims(probe,axis=0), gallery_features),axis=None)
-      ranked_ids = ids_camB[rank_p]
-      pos = np.where(ranked_ids == ids_camA[idx])
-      cmc_curve[pos[0][0]]+=1
-
-      y_true = np.zeros(np.shape(ids_camB))
-      y_true[np.where(ids_camB == ids_camA[idx])] = 1
-      y_pred = 1/(scipy.spatial.distance.cdist(np.expand_dims(probe,axis=0), gallery_features))
-      y_pred = np.squeeze(y_pred)
-      
-      ap = average_precision_score(y_true, y_pred)
-      ap_array.append(ap)
-
-    cmc_curve = np.cumsum(cmc_curve)/probe_features.shape[0]
-
-    import matplotlib
-    matplotlib.use('Agg')
-    from matplotlib import pyplot
-    pyplot.title('CMC Curve - R1: %.2f / mAP: %.2f'%(cmc_curve[0]*100, np.mean(ap_array)*100) )
-    pyplot.ylabel('Recognition Rate (%)')
-    pyplot.xlabel('Rank')
-    pyplot.plot(cmc_curve,label = curve_label, color=curve_color)
-    pyplot.legend(loc='upper left')
-    pyplot.savefig(name+'.png')
-    pyplot.close()
-    return cmc_curve, cmc_curve[0], np.mean(ap_array)
-'''
-
-
-def generate_cmc_curve(feat_model, dataset, name='cmc_curve', image_size =(224,320),metrics = ['r1, mAP'], curve_label = 'Test', curve_color='red' ):
-    test_dataA = dataset.content_array('camA')[0]
-    test_dataB = dataset.content_array('camB')[0]
-    test_identA = dataset.content_array('camA')[1]
-    test_identB = dataset.content_array('camB')[1]
-
-    camA_set0 = []
-    camB_set0 = []
-      
-    # feat_model = new_resnet.feat_net((224,320)) # prev layer
-    # feat_model._make_predict_function()    # https://github.com/keras-team/keras/issues/6462
-    # feat_model.load_weights(feat_path)
-
-    for pA in test_dataA:
-      imgA = dataset.get_image(pA)
-      imgA = Image.fromarray(imgA)
-      imgA = imgA.resize((image_size[1], image_size[0]))
-      camA_set0.append(np.array(imgA))
-
-    camA_set = np.array(camA_set0)
-
-    for pB in test_dataB:
-      imgB = dataset.get_image(pB)
-      imgB = Image.fromarray(imgB)
-      imgB = imgB.resize((image_size[1], image_size[0]))
-      camB_set0.append(np.array(imgB)) 
-
-    camB_set = np.array(camB_set0)
-
-    ids_camA = np.array(test_identA)
-    ids_camB = np.array(test_identB)
-
-    probe_features = feat_model.predict(camA_set)
-    gallery_features = feat_model.predict(camB_set)
-
-    cmc_curve = np.zeros(gallery_features.shape[0])
-    ap_array = []
-
-    for idx, probe in enumerate(probe_features):
-
-      rank_p = np.argsort(scipy.spatial.distance.cdist(np.expand_dims(probe,axis=0), gallery_features),axis=None)
-      ranked_ids = ids_camB[rank_p]
-      pos = np.where(ranked_ids == ids_camA[idx])
-      cmc_curve[pos[0][0]]+=1
-
-      y_true = np.zeros(np.shape(ids_camB))
-      y_true[np.where(ids_camB == ids_camA[idx])] = 1
-      y_pred = 1/(scipy.spatial.distance.cdist(np.expand_dims(probe,axis=0), gallery_features) + 1e-8)
-      y_pred = np.squeeze(y_pred)
-
-      # print('min: ',(scipy.spatial.distance.cdist(np.expand_dims(probe,axis=0), gallery_features)).min())
-
-      ap = average_precision_score(y_true, y_pred)
-      ap_array.append(ap)
-
-    cmc_curve = np.cumsum(cmc_curve)/probe_features.shape[0]
-
-    import matplotlib
-    matplotlib.use('Agg')
-    from matplotlib import pyplot
-    pyplot.title('CMC Curve - R1: %.2f / mAP: %.2f'%(cmc_curve[0]*100, np.mean(ap_array)*100) )
-    pyplot.ylabel('Recognition Rate (%)')
-    pyplot.xlabel('Rank')
-    pyplot.plot(cmc_curve,label = curve_label, color=curve_color)
-    pyplot.legend(loc='upper left')
-    pyplot.savefig(name+'.png')
-    pyplot.close()
-    return cmc_curve, cmc_curve[0], np.mean(ap_array)
