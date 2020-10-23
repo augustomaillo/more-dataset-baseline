937ce52eb42352878b76ced01124e6578b6f1529
augustomaillo <augusto.maillo@gmail.com>
2020-10-23 10:41:31 -0300

    Update training example

--- main_train.py
+++ main_train.py
@@ -1,54 +1,61 @@
-from model.Quadruplet import quadruplet_net
-from model.Triplet import triplet_net
-from model.AuxLayers import embedding, classification_layer, classification_net
-
+from model.AuxLayers import classification_layer_baseline, classification_net
 from utils.dataset import Dataset
+from model.Resnet50_LastStride1 import ResNet50 as ResNet50_LastStride
+from utils.Trainer import train_classifier, train_trinet, train_quadnet, train_msml
 
-# from model.moto_ResNet50 import feat_net as std_feat_net
-from model.modified_resnet50 import ResNet50 as modified_feat_net
-
-models_folder = 'saved_models'
+from model.general_model import general_net_center 
+from model.general_model import centernet
 
 import numpy as np
 import os 
-
 np.random.seed(2301)
 
-TrainData = Dataset('datasets/train_23_04.hdf5')
-# TestData = Dataset('test.hdf5')
+models_folder = '/mnt/users/augusto.maillo/slurm-jupyter/saved_models/'
 
-feat_model = modified_feat_net((224,320,3), TrainData.ident_num()) # BASELINE
-feat_model.load_weights(os.path.join(models_folder,'pesos_RESNET50_BASELINE.h5'), by_name=True) 
-feat_model.summary()
-identity_model = classification_layer(TrainData.ident_num(), bias = False)
+MoRe_path = "/shared/sense/Dados_IC/Motorcycle ReID/MoRe_Final.hdf5"
+MoRe = Dataset(MoRe_path, to_bgr = True)
 
+INPUT_SHAPE = (256, 256)
 
-# quadrupletNet = quadruplet_net(feat_model, identity_model, (224,320))
+feat_model = ResNet50_LastStride(input_shape = (INPUT_SHAPE[0], INPUT_SHAPE[1], 3)) # BASELINE STRIDE = 1
+feat_model.load_weights(os.path.join(models_folder,'RESNET50_ORIGINAL_WEIGHTS.h5'), by_name=True)
+feat_model._make_predict_function()
 
-clsNet  = classification_net(feat_model, identity_model, (224,320))
+identity_model = classification_layer_baseline(MoRe.ident_num('train'), BN = True) # Loading BN
+clsNet  = classification_net(feat_model, identity_model, img_shape = INPUT_SHAPE)
 
-tripletNet = triplet_net(feat_model, identity_model, (224,320))
+# train_classifier(
+#     feat_model = feat_model,
+#     model = clsNet,
+#     dataset = MoRe,
+#     modelpath =  'tsting_cls.hdf5',
+#     models_folder = models_folder,
+#     epochs = 1,
+#     batch_size = 32, 
+#     img_size = INPUT_SHAPE,
+#     label_smoothing = True,
+#     wlr = True,
+#     BN = True
+# )
 
-from utils.Trainer import train_classifier, train_trinet, train_quadnet
+clsNet.load_weights(os.path.join(models_folder, 'tsting_cls.hdf5'))
 
-train_classifier(
-    feat_model,
-    clsNet,
-    'datasets/train_23_04.hdf5',
-    'datasets/val_23_04.hdf5',
-    'cls_newdiv_23_04.hdf5',
-    train_flag = False,
-    epochs = 120
-)
+centernet = centernet(MoRe.ident_num('train'), 2048)
+
+gen_model = general_net_center(feat_model, identity_model, centernet, INPUT_SHAPE)
 
 train_trinet(
-    feat_model, 
-    tripletNet, 
-    'datasets/train_23_04.hdf5',
-    'datasets/val_23_04.hdf5',
-    'trinet_newdiv_23_04_modified.hdf5',  
-    epochs = 120,
-    batch_size = 6
+    feat_model = feat_model,
+    model = gen_model,
+    dataset = MoRe,
+    modelpath = 'tsting_ML.hdf5',
+    models_folder = models_folder,
+    epochs = 1,
+    batch_size = 6, 
+    img_size = INPUT_SHAPE,
+    aug = True,
+    label_smoothing = True,
+    wlr = True,
+    BN = True, 
+    CenterLoss = True
 )
-
-
