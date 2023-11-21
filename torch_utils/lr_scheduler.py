6e8c2c1453a63fb9baa1687563f1890242056135
augustomaillo <augusto.maillo@gmail.com>
2023-11-21 16:32:57 -0300

    Testing new schedule approches

--- torch_utils/lr_scheduler.py
+++ torch_utils/lr_scheduler.py
@@ -2,19 +2,33 @@ from typing import Callable, List, Union
 from torch.optim.lr_scheduler import LambdaLR
 from torch.optim.optimizer import Optimizer
 
+# def warmup(current_step: int):
+#     current_step = current_step+1
+#     if current_step <= 10:
+#         return 3.5e-5*(current_step)
+#     elif current_step <= 40:
+#         return 3.5e-4
+#     elif current_step <= 70:
+#         return 3.5e-5
+#     elif current_step <= 120:
+#         return 3.5e-6
+#     else:
+#         return 3.5e-7 
+    
 def warmup(current_step: int):
     current_step = current_step+1
     if current_step <= 10:
-       return 3.5e-5*(current_step)
-    elif current_step <= 40:
-        return 3.5e-4
-    elif current_step <= 70:
         return 3.5e-5
-    elif current_step <= 120:
+    elif current_step <= 20:
+        return 1e-5
+    elif current_step <= 70:
         return 3.5e-6
-    else:
-        return 3.5e-7
+    elif current_step <= 120:
+        return 0.5e-6
+    return 3.5e-7 
     
+
+
 class WarmupLR(LambdaLR):
     def __init__(self, optimizer: Optimizer):
         super().__init__(optimizer=optimizer, lr_lambda=warmup)
